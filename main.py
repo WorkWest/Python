272d860c443c928c8d9cb4cdb745ec7c80b88ccd
WorkWest <113034290+WorkWest@users.noreply.github.com>
2022-09-08 17:29:08 +0000

    python

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ user_guess = pyip.inputNum("Guess a number between 1 and 100: ")
 
 machine_number = random.randrange(1,100)
 
-guesses = 0
+counter = 0
 
-while user_guess != machine_number:
-  while user_guess < machine_number:
-    print("Too low!")
-    user_guess = pyip.inputNum("Guess again: ")
-    guesses += 1
-  while user_guess > machine_number:
-    print("Too High!")
-    user_guess = pyip.inputNum("Guess again: ")
-    guesses += 1
+play_again = True
 
-print(f"You did it in {guesses} tries!")
+while play_again == True:
+  while user_guess != machine_number:
+    while user_guess < machine_number:
+      print("Too low!")
+      user_guess = pyip.inputNum("Guess again: ")
+      counter += 1
+    while user_guess > machine_number:
+      print("Too High!")
+      user_guess = pyip.inputNum("Guess again: ")
+      counter += 1
+    print(f"You did it in {counter} tries!")
+    play_again = input("Would you like to play again? (True or False)")
+  
