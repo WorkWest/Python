8c32bbb6ce48dc34b998ee3ecfa37628529f367e
WorkWest <113034290+WorkWest@users.noreply.github.com>
2022-09-08 18:50:59 +0000

    python

--- _Labs.md
+++ _Labs.md
@@ -223,5 +223,14 @@ for x in range(0,loopthis):
 ### **Lab 3.11: Fizzy Lifting Drink**
 
 ```python
+for x in range(101):
+  if (x % 3 == 0) and (x % 5 ==0):
+    print("fizzbuzz")
+  elif x % 3 == 0:
+    print("fizz")
+  elif x % 5 == 0:
+    print("buzz")
+  else:
+    print(x)
 
 ```
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-ip_addr = input("Please input an IP Address: ")
-
-subnet_mask = input("Please input a Subnet Mask: ")
-
-broken_ip = ip_addr.split('.')
-broken_subnet = subnet_mask.split('.')
-loopthis = 256 - int(broken_subnet[3])
-
-print(broken_ip)
-print(broken_subnet)
-print(loopthis)
-
-for x in range(0,loopthis):
-  print(x)
-  
+for x in range(101):
+  if (x % 3 == 0) and (x % 5 ==0):
+    print("fizz buzz")
+  elif x % 3 == 0:
+    print("fizz")
+  elif x % 5 == 0:
+    print("buzz")
+  else:
+    print(x)
